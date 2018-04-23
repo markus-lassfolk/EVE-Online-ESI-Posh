bfaeeca721df4fdd52ef5c118687d4eddb51ab63
Markus Lassfolk <markus.lassfolk@truesec.se>
2018-04-22 22:02:36 -0400

    fixes

--- CreateMods/Create Modules.ps1
+++ CreateMods/Create Modules.ps1
@@ -5,65 +5,41 @@ $ModSwagger = get-content 'C:\SwaggerSpecs\LatestTranq.json' | ConvertFrom-Json
 $ESIHost = $ModSwagger.host
 
 
-$AllPathEndpoints = $ModSwagger.paths | get-member | where MemberType -eq "NoteProperty" | select -First 1 
+$AllPathEndpoints = $ModSwagger.paths | get-member | where MemberType -eq "NoteProperty" 
 
-foreach ($PathEndpoint in $AllPathEndpoints) { 
+$Teeest = foreach ($PathEndpoint in $AllPathEndpoints) { 
 
-    
     $CurrentEndPoint = $ModSwagger.paths.($PathEndpoint.name)
     $Method = ($CurrentEndPoint | get-member | where MemberType -like NoteProperty).Name 
     $CurrentEndPointDetails = $ModSwagger.paths.($PathEndpoint.name).$Method
 
-
     $ESIName = ($PathEndpoint.Name).TrimStart("/").TrimEnd("/").replace("/"," ")
     $ESIName = (Get-Culture).TextInfo.ToTitleCase( $ESIName )
-    $ESIFunctionName = $Method+"-EVE"+$ESIName.trim().replace(" ","")
-    
-    $ESIParameters
-    
-    
-    foreach ($ESIParameter in $CurrentEndPointDetails.parameters.'$ref') { 
-    
-    $ESIParameter.ToString().split("/")[-1]
+    $ESIFunctionName = $Method+"-EVE"+$ESIName.trim().replace(" ","").replace("{","").Replace("}","")
 
-    $ModSwagger.parameters
-
-    
+    $ESIParameters = foreach ($ESIParameter in $CurrentEndPointDetails.parameters) { 
+        if ($ESIParameter.'$ref' -like "*#/parameters/*") { 
+        $ModSwagger.parameters.$($ESIParameter.'$ref'.ToString().split("/")[-1])
+        }
+        else {$ESIParameter}
     } 
 
-
-
     $NewFunction = @{
-    
     'FunctionName' = $ESIFunctionName 
     'ESIMethod' = $Method
     'ESIPath' = $PathEndpoint.Name
     'ESIParameters' = $ESIParameters
-    $ESITags
-    $ESISUmmary
-    $ESIDescription
-    $ESIOperationsID
-    $ESIResponses
-    $ESICachedSeconds
-
-    
-    
+    'ESITags' = $CurrentEndPointDetails.tags
+    'ESISummary' = $CurrentEndPointDetails.summary
+    'ESIDescription' = $CurrentEndPointDetails.description
+    'ESIOperationsID' = $CurrentEndPointDetails.operationId
     }
+   New-Object -TypeName PSObject -ArgumentList $NewFunction
 
 }
 
-$ESIFunctionName 
-$ESIMethod
-$ESIPath
-$ESIParameters
-$ESITags
-$ESISUmmary
-$ESIDescription
-$ESIOperationsID
-$ESIResponses
-$ESICachedSeconds
 
-
+$Teeest | where ESITags -like "*Unive*" | select -First 1 
 
 
 
